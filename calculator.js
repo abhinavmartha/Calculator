487077bdfe08c2d91845e8a758bcde5802bb9eb0
abhinavmartha <abhinavmartha35@gmail.com>
2023-02-16 10:59:11 +0530

    Initial Commmit

---
+++ calculator.js
@@ -0,0 +1,27 @@
+function add() {
+    var num1 = parseFloat(document.getElementById("input1").value);
+    var num2 = parseFloat(document.getElementById("input2").value);
+    var result = num1 + num2;
+    document.getElementById("result").value = result;
+  }
+  
+  function subtract() {
+    var num1 = parseFloat(document.getElementById("input1").value);
+    var num2 = parseFloat(document.getElementById("input2").value);
+    var result = num1 - num2;
+    document.getElementById("result").value = result;
+  }
+  
+  function multiply() {
+    var num1 = parseFloat(document.getElementById("input1").value);
+    var num2 = parseFloat(document.getElementById("input2").value);
+    var result = num1 * num2;
+    document.getElementById("result").value = result;
+  }
+  
+  function divide() {
+    var num1 = parseFloat(document.getElementById("input1").value);
+    var num2 = parseFloat(document.getElementById("input2").value);
+    var result = num1 / num2;
+    document.getElementById("result").value = result;
+  }  
